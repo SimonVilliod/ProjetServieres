97e1a84ddccbe8cb85ed6aad8b8dee028e334c78
Simon <simon.villiod@gmail.com>
2016-02-24 00:53:18 +0100

    Test commit

---
+++ MethodeAjouterEvenement.java
@@ -0,0 +1,16 @@
+public static void EntreeValeursTableau(int tableau[][], String[] libelle){
+		
+	int i = 0;
+	
+	while(Test si valeur != -1 ou garder i en mémoire){
+	    i++;
+	}
+		
+	    System.out.print("Entrez le jour de l'événement: ");
+		tableau[i][0] = entree.nextInt();
+		System.out.print("Entrez le mois de l'événement: ");
+		tableau[i][1] = entree.nextInt();  
+		
+	    libelle[i] = //méthode pour ajouter un libellé 
+		    	
+}
